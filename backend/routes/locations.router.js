6446d1c9b20999ff40f909c16877e634d6fa821f
MarcBejjani <bejjanimarc@gmail.com>
2023-04-30 18:12:29 +0300

    - Deleted previous iteration

--- backend/routes/locations.router.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router()
-
-const locationsController = require("../controller/locations.controller")
-router.get("/", locationsController.getAll)
-router.get("/:id", locationsController.getById)
-
-module.exports = router
